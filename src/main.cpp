5e0f93c73ed1d7330ff6b76d244a412eb0c1566c
Frédéric Bonjour <frederic.bonjour@reymann.com>
2018-02-19 18:03:36 +0100

    Added comment to explain MQTT topics and messages.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,47 @@
+/*
+MQTT topics & messages
+
+# Vocabulary
+
+- T : device Type
+- N : device Number
+- G : device Group
+- I : device ID
+
+# From Luciol Bridge to Devices (B2D)
+
+B2D/T/<type>/<cmd>
+B2D/N/<number>/<cmd>
+B2D/G/<group>/<cmd>
+B2D/I/<group>/<cmd>
+B2D/all/<cmd>
+
+## Examples
+
+  Topic             |  Message contents  |  Signification
+--------------------+--------------------+-------------------------------------------------------------
+B2D/T/luciol/app    | 3                  | Switch to 'app' '3' on 'luciol' devices
+B2D/N/4/app         | 7                  | Switch to 'app' '7' on device number '4'
+B2D/G/1/ambience    | 4                  | Switch to 'ambience' '4' on devices in group '1'
+B2D/all/ambience    | 5                  | Switch to 'ambience' '5' on ALL devices
+B2D/N/4/resume      |                    | Starts/resume animations till next step on device number '4'
+B2D/I/147/locate    |                    | Asks device with ID '147' to come out
+--------------------+--------------------+-------------------------------------------------------------
+
+# From Luciol Devices to Bridge (D2B)
+
+D2B/<number>/alive
+D2B/<number>/paused
+
+## Examples
+
+  Topic             |  Message contents  |  Signification
+--------------------+--------------------+-------------------------------------------------------------
+D2B/alive           | 7                  | Device number '7' is alive
+D2B/paused          | 4                  | Device number '4' has reached its animation end
+--------------------+--------------------+-------------------------------------------------------------
+*/
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
